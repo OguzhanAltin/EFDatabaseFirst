bc263b0f9ae84bae5c7eb43775ce0edf91ed2b4a
Oğuzhan Altın <oguz_altin@hotmail.com>
2019-03-31 01:12:06 +0300

    Add project files.

---
+++ EFDatabaseFirst/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace EFDatabaseFirst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnEmployees = new System.Windows.Forms.Button();
+            this.btnOrders = new System.Windows.Forms.Button();
+            this.btnTotalPrice = new System.Windows.Forms.Button();
+            this.btnProductAndSupplier = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnExit.Location = new System.Drawing.Point(825, 402);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(99, 35);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnClear.Location = new System.Drawing.Point(19, 402);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(97, 35);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnEmployees
+            // 
+            this.btnEmployees.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEmployees.Location = new System.Drawing.Point(714, 289);
+            this.btnEmployees.Name = "btnEmployees";
+            this.btnEmployees.Size = new System.Drawing.Size(154, 42);
+            this.btnEmployees.TabIndex = 11;
+            this.btnEmployees.Text = "Some Information From The Employees Tables";
+            this.btnEmployees.UseVisualStyleBackColor = true;
+            this.btnEmployees.Click += new System.EventHandler(this.btnEmployees_Click);
+            // 
+            // btnOrders
+            // 
+            this.btnOrders.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOrders.Location = new System.Drawing.Point(484, 289);
+            this.btnOrders.Name = "btnOrders";
+            this.btnOrders.Size = new System.Drawing.Size(154, 43);
+            this.btnOrders.TabIndex = 10;
+            this.btnOrders.Text = "Some Informations From The Orders Tables ";
+            this.btnOrders.UseVisualStyleBackColor = true;
+            this.btnOrders.Click += new System.EventHandler(this.btnOrders_Click);
+            // 
+            // btnTotalPrice
+            // 
+            this.btnTotalPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTotalPrice.Location = new System.Drawing.Point(313, 290);
+            this.btnTotalPrice.Name = "btnTotalPrice";
+            this.btnTotalPrice.Size = new System.Drawing.Size(85, 42);
+            this.btnTotalPrice.TabIndex = 9;
+            this.btnTotalPrice.Text = "Total Price By Category";
+            this.btnTotalPrice.UseVisualStyleBackColor = true;
+            this.btnTotalPrice.Click += new System.EventHandler(this.btnTotalPrice_Click);
+            // 
+            // btnProductAndSupplier
+            // 
+            this.btnProductAndSupplier.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnProductAndSupplier.Location = new System.Drawing.Point(90, 290);
+            this.btnProductAndSupplier.Name = "btnProductAndSupplier";
+            this.btnProductAndSupplier.Size = new System.Drawing.Size(140, 42);
+            this.btnProductAndSupplier.TabIndex = 8;
+            this.btnProductAndSupplier.Text = "Product And Supplier Information ";
+            this.btnProductAndSupplier.UseVisualStyleBackColor = true;
+            this.btnProductAndSupplier.Click += new System.EventHandler(this.btnProductAndSupplier_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 11);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(905, 245);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 449);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEmployees);
+            this.Controls.Add(this.btnOrders);
+            this.Controls.Add(this.btnTotalPrice);
+            this.Controls.Add(this.btnProductAndSupplier);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Linq To Entity";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnEmployees;
+        private System.Windows.Forms.Button btnOrders;
+        private System.Windows.Forms.Button btnTotalPrice;
+        private System.Windows.Forms.Button btnProductAndSupplier;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ EFDatabaseFirst/Form1.cs
@@ -0,0 +1,108 @@
+﻿using EFDatabaseFirst.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.SqlServer;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFDatabaseFirst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        NorthwindEntities db = new NorthwindEntities();
+
+        public void Cleaner()
+        {
+            if (dataGridView1.DataSource != null)
+            {
+                dataGridView1.DataSource = "";
+            }
+        }
+        private void btnProductAndSupplier_Click(object sender, EventArgs e)
+        {
+            Cleaner();
+
+            dataGridView1.DataSource = db.Products.OrderByDescending
+                (d => d.UnitPrice).Select(x => new
+                {
+                    SupplierCompanyName = x.Supplier.CompanyName,
+                    SupplierContactName = x.Supplier.ContactName,
+                    x.ProductName,
+                    x.UnitPrice,
+                    x.UnitsInStock
+                }).ToList();
+        }
+
+        private void btnTotalPrice_Click(object sender, EventArgs e)
+        {
+            Cleaner();
+
+            dataGridView1.DataSource = db.Products.OrderByDescending
+                (i => i.UnitPrice).
+                GroupBy(x => x.Category.CategoryName).Select(y => new
+                {
+                    CategoryName = y.Key,
+                    TotalPrice = y.Sum(u => u.UnitPrice)
+                }).ToList();
+        }
+
+        private void btnOrders_Click(object sender, EventArgs e)
+        {
+            Cleaner();
+
+            dataGridView1.DataSource = db.Orders.Select(o => new
+            {
+                o.Customer.CompanyName,
+                FullName = o.Employee.FirstName + " " + o.Employee.LastName,
+                o.OrderID,
+                o.OrderDate,
+                ShipperCompanyName = o.Shipper.CompanyName
+            }).ToList();
+        }
+
+        private void btnEmployees_Click(object sender, EventArgs e)
+        {
+            Cleaner();
+
+            dataGridView1.DataSource = db.Employees.OrderBy
+                (o => o.BirthDate).Select(x => new
+                {
+                    FullName = x.FirstName + " " + x.LastName,
+                    x.BirthDate,
+                    Age = SqlFunctions.DateDiff("Year", x.BirthDate, DateTime.Now)
+                }).ToList();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Cleaner();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show
+                  ("Do you want to quit?", "EFDatabaseFirst", MessageBoxButtons.YesNo);
+
+            if (dialog == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+            else if (dialog == DialogResult.No)
+            {
+
+            }
+        }
+    }
+
+}
